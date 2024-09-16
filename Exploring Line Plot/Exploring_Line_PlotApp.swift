aa34bc71111e30f04f8b5058ba0fef0668bb2f46
uwi0 <kakapoweb@gmail.com>
2024-09-16 17:31:32 +0700

    Initial Commit

---
+++ Exploring Line Plot/Exploring_Line_PlotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exploring_Line_PlotApp.swift
+//  Exploring Line Plot
+//
+//  Created by dwi prasetyo on 16/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Exploring_Line_PlotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
